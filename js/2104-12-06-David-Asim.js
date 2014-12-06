7bd435b8df4e7f287f60cd539bfe7d0e2718731c
davrossuk <speaktodave@gmail.com>
2014-12-06 16:17:41 +0000

    davids page

---
+++ js/2104-12-06-David-Asim.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+   $(".planet").click(function() {
+        $(this).effect('bounce', {times:3}, 500);
+     
+   });
+
+   $(".planet-right").click(function() {
+        $(this).effect('bounce', {times:3}, 500);
+     
+   });
+  
+  
+  
+  
+});
